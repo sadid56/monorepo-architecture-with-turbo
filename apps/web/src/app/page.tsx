2b3307670832bdbdc15e36b73daa9ef9ed9363a2
sadid56 <sadidhasan56@gmail.com>
2025-10-23 11:24:18 +0600

    Fix: remove nested git repos and add app code

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@my-repo/ui";
+import { BiArrowBack } from "react-icons/bi";
+
+export default function Home() {
+  return (
+    <Button>
+      Hi <BiArrowBack />
+    </Button>
+  );
+}
